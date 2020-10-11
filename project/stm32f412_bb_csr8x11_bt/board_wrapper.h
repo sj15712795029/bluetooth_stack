b390790e207a231f94f23c0e5d20c4db076914ac
yuzhongjun <124756828@qq.com>
2020-10-11 14:22:15 +0800

    修改：把board_wrapper放到当前工程文件夹中，把每个工程区分开

---
+++ project/stm32f412_bb_csr8x11_bt/board_wrapper.h
@@ -0,0 +1,16 @@
+#ifndef BOARD_WRAPPER_H_H_H
+#define BOARD_WRAPPER_H_H_H
+
+#include "hw_misc.h"
+#include "hw_uart.h"
+#include "hw_systick.h"
+#include "hw_led.h"
+#include "hw_button.h"
+#include "hw_oled.h"
+#include "hw_sht2x.h"
+#include "hw_spi_flash.h"
+#include "usb_lib.h"
+#include "fs_wrapper.h"
+
+
+#endif
